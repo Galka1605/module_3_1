931235cd838ab0fc602914979ea09558c0454778
Galka1605 <gal.crilosova@yandex.ru>
2024-08-16 11:09:35 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -4,19 +4,18 @@ def count_calls():
     calls += 1
 
 def string_info(string):
-    print((len(string), string.upper(), string.lower()))
     count_calls()
+    return((len(string), string.upper(), string.lower()))
 
 def is_contains(string, list_to_search):
-    string_lower = string.lower()
     list_to_search_lower = []
     for i in list_to_search:
         list_to_search_lower.append(i.lower())
-    if string_lower in list_to_search_lower:
-        print(True)
-    else:
-        print(False)
     count_calls()
+    if string.lower() in list_to_search_lower:
+        return (True)
+    else:
+        return(False)
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
